cd6045362c95a4d85c5296c273f330b5d20fe9f6
robertgrzonka <robert@theguys.sh>
2019-04-03 20:56:28 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,12 +1,4 @@
 const Conf = require('conf')
-const pkg = require('./package.json')
-const colorPalette = require('./colorPalette.js')
-
 const config = new Conf()
-config.store = {
-  name: pkg.name,
-  private: true,
-  colors: Object.entries(colorPalette)
-}
 
 module.exports = config
