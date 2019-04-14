497096a2af9c1a00ec7bbfab0be5cde8a0cacd17
robertgrzonka <hello@robertgrzonka.pl>
2019-04-14 16:26:48 +0200

    feat(collo.delete)
    * add `collo.delete`
    * small fixes

--- collo.js
+++ collo.js
@@ -25,31 +25,42 @@ Object.defineProperties(collo, {
   },
 
   // set new color as an Array or overwrite existing ones
-  addColor: {
+  add: {
     set (color) {
-      if (color.length !== 2) chalk`Array should contain exactly two items: key and value. {bold.red Example}: ['black', '#000000']`
+      if (color.length !== 2) throw new Error(`Array should contain exactly two items: key and value. ${chalk.bold.red('Example')}: ['black', '#000000']`)
 
       const [ key, value ] = color
-      if (this.config.get(`colors.${key}`)) chalk`This color already exists. If you want to overwrite it use {bold.red editColor}`
+      if (this.config.get(`colors.${key}`)) throw new Error(`This color already exists. If you want to overwrite it use ${chalk.bold.red('edit')}`)
 
       const regex = /^\#[0-9a-zA-Z]{6}/
-      if (!regex.test(value)) chalk`Value have to be in HEX format. {bold.red Example}: #000000`
+      if (!regex.test(value)) throw new Error(`Value have to be in HEX format. ${chalk.bold.red('Example')}: #000000`)
 
       this.config.set(`colors.${key}`, value)
-      return this.config.get(`colors.${key}`)
+      console.log(`Added color ${chalk.bold(key)} with value: ${chalk.bold(value)}`)
     }
   },
 
-  editColor: {
+  edit: {
     set (color) {
+      if (color.length !== 2) throw new Error(`Array should contain exactly two items: key and value. ${chalk.bold.red('Example')}: ['black', '#000000']`)
+
       const [ key, value ] = color
-      if (!this.colors[ key ]) chalk`This color doesn't exist. If you want to add new collor use {bold addColor}`
+      if (!this.colors[ key ]) throw new Error(`This color doesn't exist. If you want to add new collor use ${chalk.bold('add')}`)
 
       const regex = /^\#[0-9a-zA-Z]{6}/
-      if (!regex.test(value)) chalk`Value have to be in HEX format. {bold.red Example}: #000000`
+      if (!regex.test(value)) throw new Error(`Value have to be in HEX format. ${chalk.bold.red('Example')}: #000000`)
 
       this.config.set(`colors.${key}`, value)
-      return this.config.get(`colors.${key}`)
+      console.log(`New value for ${chalk.bold(key)} is: ${chalk.bold(value)}`)
+    }
+  },
+
+  delete: {
+    set (color) {
+      if (!this.colors[ color ])
+        throw new Error(`This color doesn't exist. Use ${chalk.underline('collo.colors')} and try again using name or index of appropriate color.`)
+      this.config.delete(`colors.${color}`)
+      console.log(chalk.underline(`${color.toUpperCase()} was deleted.`))
     }
   }
 })
--- collo.test.js
+++ collo.test.js
@@ -2,18 +2,19 @@ const collo = require('./collo')
 
 describe('collo object', () => {
 
-  afterAll(() => {
-    collo.config.delete('colors.testColorJest')
-  })
-
   test('adds new color', () => {
-    collo.addColor = [ 'testColorJest', '#FF1493' ]
+    collo.add = [ 'testColorJest', '#FF1493' ]
     expect(collo.colors.testColorJest).toBeTruthy()
   })
 
   test('edits existing color', () => {
-    collo.editColor = [ 'black', '#000000' ]
-    expect(collo.colors.black).toEqual('#000000')
+    collo.edit = [ 'testColorJest', '#FF4091' ]
+    expect(collo.colors.testColorJest).toEqual('#FF4091')
+  })
+
+  test('deletes color', () => {
+    collo.delete = 'testColorJest'
+    expect(collo.colors.testColorJest).toBeFalsy()
   })
 
   test('returns path', () => {
@@ -24,7 +25,7 @@ describe('collo object', () => {
     expect(collo.colors).toEqual(collo.config.get('colors'))
   })
 
-  test('had good RegEx', () => {
+  test('has working RegEx', () => {
     const regex = /^#[0-9a-zA-Z]{6}/
     expect(regex.test('#000')).toBeFalsy()
     expect(regex.test('#000000')).toBeTruthy()
